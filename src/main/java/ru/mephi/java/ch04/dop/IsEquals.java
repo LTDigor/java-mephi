7be51296f38e18987c8cbe2ec3577ec970f9431c
LTDigor <v.d.olennikov@tcsbank.ru>
2022-09-13 17:53:20 +0300

    add method

--- src/main/java/ru/mephi/java/ch04/dop/IsEquals.java
+++ src/main/java/ru/mephi/java/ch04/dop/IsEquals.java
@@ -3,6 +3,6 @@ package ru.mephi.java.ch04.dop;
 public class IsEquals {
 
     public static boolean isEquals(Object obj, Object other) {
-        return false;
+        return obj == other;
     }
 }
