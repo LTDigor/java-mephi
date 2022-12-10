ce9f5b1d0a2cda2bc1bb74b88ed4d331c5c35a0b
LTDigor <ovd2001@mail.ru>
2022-12-10 07:31:40 +0300

    edit task

--- src/main/java/ru/mephi/java/ch07/sec09/TestMapCounters.java
+++ src/main/java/ru/mephi/java/ch07/sec09/TestMapCounters.java
@@ -8,26 +8,27 @@ public class TestMapCounters {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
 
-        // update counter with merge
-        map.merge("wordMerge", 1, Integer::sum);
+        for(int i = 0; i < 2; i++) {
+            // update counter with merge
+            map.merge("wordMerge", 1, Integer::sum);
 
-        // update counter with contains
+            // update counter with contains
             map.put("wordContains", 1 + (map.containsKey("wordContains") ? map.get("wordGet") : 0));
 
-        // update counter with get
-        if (map.get("wordGet") != null) {
-            map.put("wordGet", map.get("wordGet") + 1);
-        }
-        else {
-            map.put("wordGet", 1);
-        }
+            // update counter with get
+            if (map.get("wordGet") != null) {
+                map.put("wordGet", map.get("wordGet") + 1);
+            } else {
+                map.put("wordGet", 1);
+            }
 
-        // update counter with getOrDefault
-        map.put("wordGetOrDefault", 1 + map.getOrDefault("wordGetOrDefault", 0));
+            // update counter with getOrDefault
+            map.put("wordGetOrDefault", 1 + map.getOrDefault("wordGetOrDefault", 0));
 
-        // update counter with putIfAbsent
-        map.putIfAbsent("wordPutIfAbsent", 0);
-        map.put("wordPutIfAbsent", map.get("wordPutIfAbsent") + 1);
+            // update counter with putIfAbsent
+            map.putIfAbsent("wordPutIfAbsent", 0);
+            map.put("wordPutIfAbsent", map.get("wordPutIfAbsent") + 1);
+        }
 
         System.out.println(map);
     }
