327a504bfa08cfbea464325296429d6f0b6c4812
v.d.olennikov <v.d.olennikov@tinkoff.ru>
2023-01-14 09:29:44 +0300

    done ch06 sec16

--- src/main/java/ru/mephi/java/ch06/sec16/Test.java
+++ src/main/java/ru/mephi/java/ch06/sec16/Test.java
@@ -1,12 +1,30 @@
 package ru.mephi.java.ch06.sec16;
 
+import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.List;
 
 public class Test {
-    public static void main(String[] args) {
-//        public static <T extends Comparable<? super T>> void sort(List<T> list);
-//        public static <T extends Object & Comparable<? super T>> T max(Collection <? extends T> coll);
-//        sort: T -> Comparable
-//        max: T -> Object
+    public static void main(String[] args) throws NoSuchMethodException {
+        Method sortMethod = Test.class.getMethod("sort", List.class);
+        Method maxMethod = Test.class.getMethod("max", Collection.class);
+
+        System.out.println(sortMethod + ": " + sortMethod.getParameters()[0].getType());
+        System.out.println(maxMethod + ": " + maxMethod.getReturnType()); // return type is Object!
     }
+
+    // sort: T -> Comparable
+    public static <T extends Comparable<? super T>> void sort(List<T> list) {
+    }
+
+    // helper method with the same signature, type param is T
+    // we cant get T type from sort method
+    public static <T extends Comparable<? super T>> void sortHelper(T list) {
+    }
+
+    // max: T -> Object
+    public static <T extends Object & Comparable<? super T>> T max(Collection<? extends T> coll) {
+        return coll.iterator().next();
+    }
+
 }
